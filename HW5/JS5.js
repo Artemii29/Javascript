0a3a3d912d5d9d2c7e7835425ef27207456a8a77
Artemii29 <artemii.kharitonov@bk.ru>
2022-04-06 13:00:31 +0300

    Add hometask 5 (1)

---
+++ HW5/JS5.js
@@ -0,0 +1,11 @@
+/*function range(start,end,step = 1){
+    let mass = []
+    for(let i = start; i <= end;i+=step){
+        mass.push(i)
+    }
+    return mass
+}
+console.log(range(3 , 9,2))
+*/
+//Задача 2
+
