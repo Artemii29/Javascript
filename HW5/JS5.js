1582428c7d35d5d598ab7481d52a0b6161d347b9
Artemii29 <artemii.kharitonov@bk.ru>
2022-04-07 20:10:32 +0300

    corrected hometask 5

--- HW5/JS5.js
+++ HW5/JS5.js
@@ -1,22 +1,31 @@
-/*function range(start,end,step = 1){
+function range(start,end,step = 1){
     let mass = []
     for(let i = start; i <= end;i+=step){
         mass.push(i)
+        if(step < 0){
+            alert("step не может быть отрицательным" )
+            return
+        }
+        if(start < end){
+        alert("Будет пустой массив")
+        }
     }
     return mass
 }
-console.log(range(3 , 9,2))
-*/
+console.log(range(12 , 1, 2))
+
 //Задача 2
+
 function checkSpam(text, ...spamWords) {
     let wordsArray = text.split(" ");
     let spamCounter = 0;
 
     for (const spamWord of spamWords) {
-        for (let i = 0; i <= wordsArray.length; i++) {
-            if (wordsArray[i] === spamWord) spamCounter++;
+        for (let i = 0; i < wordsArray.length; i++) {
+            if (wordsArray[i].toUpperCase() === spamWord.toUpperCase()) spamCounter++;
             if (spamCounter >= 5) return spamCounter;
         }
     }
     return spamCounter;
 }
+console.log(checkSpam("car super CAR ","car"))
