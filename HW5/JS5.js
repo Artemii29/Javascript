1781c3d0cee8e495fd4dd44b78d075bda2469a78
Artemii29 <artemii.kharitonov@bk.ru>
2022-04-07 10:04:54 +0300

    Add hometask 5 (2)

--- HW5/JS5.js
+++ HW5/JS5.js
@@ -8,4 +8,15 @@
 console.log(range(3 , 9,2))
 */
 //Задача 2
+function checkSpam(text, ...spamWords) {
+    let wordsArray = text.split(" ");
+    let spamCounter = 0;
 
+    for (const spamWord of spamWords) {
+        for (let i = 0; i <= wordsArray.length; i++) {
+            if (wordsArray[i] === spamWord) spamCounter++;
+            if (spamCounter >= 5) return spamCounter;
+        }
+    }
+    return spamCounter;
+}
